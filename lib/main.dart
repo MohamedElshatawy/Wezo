4bbbe035566bdb7ccec88b64fe065eb1938d8477
Mohamed Elshatawy <63617935+MohamedElshatawy@users.noreply.github.com>
2020-04-30 01:43:44 +0200

    Update main.dart
    Remove auto rotate

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,12 @@ import 'package:weather_time_app/pages/home.dart';
 import 'package:weather_time_app/pages/loading.dart';
 void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
-
-  SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-    ]);
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]); 
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
